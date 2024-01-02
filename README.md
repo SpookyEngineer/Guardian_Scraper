09756d840f7d8a30f3f1a167d672c1b3249edb85
GabrielSpookyDev <gabrielicmusa@gmail.com>
2024-01-02 17:24:18 -0300

    Refactoring project to function correctly again

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Guardian_Scraper
+
 A web scraper for https://www.theguardian.com/international that grabs all the news titles and urls, then posts them to http://localhost:8000/results <br /> <br />
 
 Used in Bonfire project
--- index.js
+++ index.js
@@ -1,37 +1,36 @@
-const PORT = 8000; // Port that will run in
+const PORT = 8080;
+const axios = require("axios");
+const cheerio = require("cheerio");
+const express = require("express");
+const app = express();
+const cors = require("cors");
 
-const axios = require('axios'); // Calling the packages
-const cheerio = require('cheerio');
-const { response } = require('express');
-const express = require('express');
-const res = require('express/lib/response');
-const cors = require('cors'); //Require cors to run in localhost
+app.use(cors());
 
-const app = express(); // Declaring the app
-app.use(cors()); // App use cors
+const baseUrl = "https://www.theguardian.com/international";
 
-const url = 'https://www.theguardian.com/international'
+app.get("/", function (req, res) {
+  res.json("This is my web-scraper");
+});
 
-app.get('/', function(req, res){
-    res.json('This is a web scraper')
-})
+app.get("/results", (req, res) => {
+  axios(baseUrl)
+    .then((response) => {
+      const html = response.data;
+      const $ = cheerio.load(html);
+      const articles = [];
 
-app.get('/results', function(req, res){
-    axios(url)  // Scraping the url
-    .then(response => {
-        const html = response.data
-        const $ = cheerio.load(html)
-        const articles = []
-        $('.fc-item__title', html).each(function(){ // The class being scraped and being pushed into variables
-            const title = $(this).text() // Grabing the text 
-            const url = $(this).find('a').attr('href') // Grabing the links
-            articles.push({
-                title,
-                url
-            })
-        })
-        res.json(articles)
-    }).catch(err => console.log(err)) // Catch in case of error
-})
+      $(".dcr-nurayl", html).each(function () {
+        const title = $(this).text();
+        const url = $(this).find("a").attr("href");
+        articles.push({
+          title,
+          url: `${baseUrl.replace("/international", "")}${url}`,
+        });
+      });
+      res.json(articles);
+    })
+    .catch((err) => console.log(err));
+});
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
+app.listen(PORT, () => console.log(`server running on PORT ${PORT}`));
